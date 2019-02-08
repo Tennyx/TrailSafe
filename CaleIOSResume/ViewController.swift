44ffef60f6a4c6c0665b0f58627935f1596d0f67
Cale Switzer <Tennyx@cales-macbook-pro.local>
2019-02-08 13:18:16 -0500

    Initial Commit

---
+++ CaleIOSResume/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CaleIOSResume
+//
+//  Created by Cale Switzer on 2/8/19.
+//  Copyright © 2019 cale. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
